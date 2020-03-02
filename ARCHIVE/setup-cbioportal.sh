0c2f7e1b64812c3392dc43ae5c0cd11422232f92
jegesm <david.visontai@complex.elte.hu>
2020-03-02 12:29:02 +0000

    v3.2.5 is up and running without admin site

--- ARCHIVE/setup-cbioportal.sh
+++ ARCHIVE/setup-cbioportal.sh
@@ -1,27 +1,27 @@
 CONFDIR=$PWD
 PREFIX=portal-dev
-CBIOPORTAL_NAME=cbioportal-dev
+MODULE_NAME=cbioportal-dev
 CBIOPORTALDB=cbioportaldb
 CBIOPORTALDB_USER=cbioportal
 CBIOPORTALDB_PW=almafa
 CBIOPORTALDBROOT_PW=almafa
-CBIOPORTALDB_URL=$PREFIX"-"$CBIOPORTAL_NAME"-mysql"
+CBIOPORTALDB_URL=$PREFIX"-"$MODULE_NAME"-mysql"
 OUTERHOST="kooplex-temp.elte.hu"
 
 ROOT="/srv/"$PREFIX
 BUILDDIR=$ROOT"/build"
-RF=$BUILDDIR/${CBIOPORTAL_NAME}
+RF=$BUILDDIR/${MODULE_NAME}
 
 mkdir -p $RF
 mkdir -p $RF/cbiolog
 mkdir -p $RF/tomcatlog
 
 SRV=$ROOT/$PREFIX
-mkdir -p $SRV/_${CBIOPORTAL_NAME}-seeddb
-mkdir -p $SRV/_${CBIOPORTAL_NAME}-studies
-mkdir -p $SRV/_${CBIOPORTAL_NAME}-db
-mkdir -p $SRV/_${CBIOPORTAL_NAME}
-mkdir -p $SRV/_${CBIOPORTAL_NAME}-install
+mkdir -p $SRV/_${MODULE_NAME}-seeddb
+mkdir -p $SRV/_${MODULE_NAME}-studies
+mkdir -p $SRV/_${MODULE_NAME}-db
+mkdir -p $SRV/_${MODULE_NAME}
+mkdir -p $SRV/_${MODULE_NAME}-install
 mkdir -p $SRV/_report
 
 CBIOPORTAL=$SRV/"_cbioportal"
@@ -32,13 +32,13 @@ mkdir -p $CBIOPORTAL
 mkdir -p $CBIOPORTAL/cbiolog
 mkdir -p $CBIOPORTAL/tomcatlog
 
-docker $DOCKERARGS volume create -o type=none -o device=$SRV/_${CBIOPORTAL_NAME}-seeddb -o o=bind ${PREFIX}-${CBIOPORTAL_NAME}-seeddb
-docker $DOCKERARGS volume create -o type=none -o device=$SRV/_${CBIOPORTAL_NAME}-db -o o=bind ${PREFIX}-${CBIOPORTAL_NAME}-db
+docker $DOCKERARGS volume create -o type=none -o device=$SRV/_${MODULE_NAME}-seeddb -o o=bind ${PREFIX}-${MODULE_NAME}-seeddb
+docker $DOCKERARGS volume create -o type=none -o device=$SRV/_${MODULE_NAME}-db -o o=bind ${PREFIX}-${MODULE_NAME}-db
 docker $DOCKERARGS volume create -o type=none -o device=$SRV/_report -o o=bind ${PREFIX}-report
-docker $DOCKERARGS volume create -o type=none -o device=$SRV/_${CBIOPORTAL_NAME}-studies -o o=bind ${PREFIX}-${CBIOPORTAL_NAME}-studies
+docker $DOCKERARGS volume create -o type=none -o device=$SRV/_${MODULE_NAME}-studies -o o=bind ${PREFIX}-${MODULE_NAME}-studies
 
 #cp Dockerfile  $RF/Dockerfile
-sed -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/"  templates/entrypoint.sh-template > $RF/entrypoint.sh
+sed -e "s/##MODULE_NAME##/${MODULE_NAME}/"  templates/entrypoint.sh-template > $RF/entrypoint.sh
 
 [ ! -d $RF/src.cbioportal ] && git clone https://github.com/jegesm/cbioportal.git $RF/src.cbioportal\
 	&& cd $RF/src.cbioportal && git checkout tags/v2.0.1\
@@ -52,7 +52,7 @@ cd $CONFDIR
 sed -e "s/##PREFIX##/${PREFIX}/" \
     -e "s/##CBIOPORTAL##/${CBIOPORTALE}/" \
     -e "s/##CBIOPORTALDB##/${CBIOPORTALDB}/" \
-    -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/g" \
+    -e "s/##MODULE_NAME##/${MODULE_NAME}/g" \
     -e "s/##CBIOPORTALDB_USER##/${CBIOPORTALDB_USER}/"\
     -e "s/##CBIOPORTALDB_PW##/${CBIOPORTALDB_PW}/"\
     -e "s/##CBIOPORTALDBROOT_PW##/${CBIOPORTALDBROOT_PW}/"\
@@ -60,18 +60,18 @@ sed -e "s/##PREFIX##/${PREFIX}/" \
 
 
 sed -e "s/##PREFIX##/${PREFIX}/" \
-    -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/" \
+    -e "s/##MODULE_NAME##/${MODULE_NAME}/" \
     -e "s/##CBIOPORTALDB##/${CBIOPORTALDB}/"\
     -e "s/##CBIOPORTALDB_URL##/${CBIOPORTALDB_URL}/"\
     -e "s/##CBIOPORTALDB_USER##/${CBIOPORTALDB_USER}/"\
     -e "s/##CBIOPORTALDB_PW##/${CBIOPORTALDB_PW}/"  templates/portal.properties_template > $RF/portal.properties 
 
-sed -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/"  templates/app.json-template > $RF/src.cbioportal/app.json 
-sed -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/"  templates/pom.xml-template > $RF/src.cbioportal/pom.xml 
-sed -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/"  templates/Dockerfile-template > $RF/Dockerfile 
+sed -e "s/##MODULE_NAME##/${MODULE_NAME}/"  templates/app.json-template > $RF/src.cbioportal/app.json 
+sed -e "s/##MODULE_NAME##/${MODULE_NAME}/"  templates/pom.xml-template > $RF/src.cbioportal/pom.xml 
+sed -e "s/##MODULE_NAME##/${MODULE_NAME}/"  templates/Dockerfile-template > $RF/Dockerfile 
 
 sed -e "s/##PREFIX##/${PREFIX}/" \
-    -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/"\
+    -e "s/##MODULE_NAME##/${MODULE_NAME}/"\
     -e "s/##CBIOPORTALDB_URL##/${CBIOPORTALDB_URL}\/$CBIOPORTALDB/"\
     -e "s/##CBIOPORTALDB##/${CBIOPORTALDB}/"\
     -e "s/##CBIOPORTALDB_USER##/${CBIOPORTALDB_USER}/"\
@@ -87,7 +87,7 @@ sed -e "s/##PREFIX##/${PREFIX}/" \
     -e "s/##OUTERHOST##/${OUTERHOST}/" templates/settings.xml_template > $RF/settings.xml 
 
 sed -e "s/##PREFIX##/${PREFIX}/" \
-    -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/" \
+    -e "s/##MODULE_NAME##/${MODULE_NAME}/" \
     -e "s/##CBIOPORTALDB##/${CBIOPORTALDB}/"\
     -e "s/##RF##/${RFS}/"\
     -e "s/##CBIOPORTALDB_USER##/${CBIOPORTALDB_USER}/"\
--- configure.sh
+++ configure.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-VERB=$1
+#VERB=$1
 
 # This is a kooplex add-on it follows it's setup architecture
 # First we need to load kooplex's config
-KOOPLEX_DIR=/srv/kooplex-config/
-. ${KOOPLEX_DIR}/lib.sh
+#KOOPLEX_DIR=/srv/kooplex-config/
+#. ${KOOPLEX_DIR}/lib.sh
 
 MODULE_NAME=cbioportal
 RF=$BUILDDIR/${MODULE_NAME}
 
 mkdir -p $RF
 
-DOCKER_HOST=$DOCKERARGS
-
-CBIOPORTAL_NAME=cbioportal
 CBIOPORTALDB=cbioportaldb
 CBIOPORTALDB_USER=cbioportal
 CBIOPORTALDB_PW=${DUMMYPASS}
 CBIOPORTALDBROOT_PW=${DUMMYPASS}
-CBIOPORTALDB_URL=$PREFIX"-"$CBIOPORTAL_NAME"-mysql"
+CBIOPORTALDB_URL=$PREFIX"-"$MODULE_NAME"-mysql"
 
 #CBIOPORTAL_HTML=$NGINX_DIR/html
 CBIOPORTAL_LOG=$LOG_DIR/${MODULE_NAME}
 CBIOPORTAL_CONF=$CONF_DIR/${MODULE_NAME}
 
+CBIOPORTAL_DIR=$SRV/"_cbioportal"
+
 DJANGO_SECRET_KEY=oyberng3ta6oh74wehx2dePiqvci7toh
-DB=${CBIOPORTAL_NAME}_admin
-DB_USER=${CBIOPORTAL_NAME}_admin
+DB=${MODULE_NAME}_admin
+DB_USER=${MODULE_NAME}_admin
 DB_PW=${DUMMYPASS}
 DBROOT_PW=${DUMMYPASS}
 LDAP_PW=${DUMMYPASS}
@@ -42,7 +41,6 @@ case $VERB in
     echo "Building cbioportal image ${PREFIX}-base"
 
     
-    CBIOPORTAL_DIR=$SRV/"_cbioportal"
     mkdir -p ${CBIOPORTAL_DIR}
     mkdir -p ${CBIOPORTAL_DIR}-admin
     mkdir -p ${CBIOPORTAL_DIR}-ldap
@@ -57,23 +55,31 @@ case $VERB in
     
     cp Dockerfile.ldap scripts/entrypoint-ldap.sh $RF/
     cp Dockerfile.admin etc/entrypoint-admin.sh $RF/ 
-    CBIOPORTALE=$(echo ${CBIOPORTAL_DIR}| sed s"/\//\\\\\//"g)
-    RFS=$(echo $RF| sed s"/\//\\\\\//"g)
+    cp etc/cgds.sql $RF/
     
-    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-admin -o o=bind ${PREFIX}-${CBIOPORTAL_NAME}-admin
-    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-ldap -o o=bind ${PREFIX}-${CBIOPORTAL_NAME}-ldap-etc
-    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-ldap -o o=bind ${PREFIX}-${CBIOPORTAL_NAME}-ldap-var
-    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-seeddb -o o=bind ${PREFIX}-${CBIOPORTAL_NAME}-seeddb
-    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-db -o o=bind ${PREFIX}-${CBIOPORTAL_NAME}-mysqldb
-    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-studies -o o=bind ${PREFIX}-${CBIOPORTAL_NAME}-studies
+    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-admin -o o=bind ${PREFIX}-${MODULE_NAME}-admin
+    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-ldap -o o=bind ${PREFIX}-${MODULE_NAME}-ldap-etc
+    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-ldap -o o=bind ${PREFIX}-${MODULE_NAME}-ldap-var
+    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-seeddb -o o=bind ${PREFIX}-${MODULE_NAME}-seeddb
+    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-db -o o=bind ${PREFIX}-${MODULE_NAME}-mysqldb
+    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_DIR}-studies -o o=bind ${PREFIX}-${MODULE_NAME}-studies
+    docker $DOCKERARGS volume create -o type=none -o device=${CBIOPORTAL_LOG} -o o=bind ${PREFIX}-${MODULE_NAME}-log
     
-#    sed -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/"  templates/entrypoint.sh-template > $RF/entrypoint.sh
+      DIR=${CBIOPORTAL_DIR}-admin
+      if [ -d $DIR/.git ] ; then
+          echo $DIR
+          #cd $DIR && git pull && cd -
+      else
+          git clone https://github.com/jegesm/cbioportal-admin.git $DIR
+      fi
+
+#    sed -e "s/##MODULE_NAME##/${MODULE_NAME}/"  templates/entrypoint.sh-template > $RF/entrypoint.sh
     
     sed -e "s/##PREFIX##/${PREFIX}/" \
-        -e "s/##CBIOPORTAL##/${CBIOPORTALE}/" \
+        -e "s,##CBIOPORTAL##,${CBIOPORTAL_DIR}," \
         -e "s/##CBIOPORTALDB##/${CBIOPORTALDB}/" \
-        -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/g" \
-        -e "s/##CBIOPORTAL_CONF##/$(echo $CBIOPORTAL_CONF | sed s"/\//\\\\\//"g)/" \
+        -e "s/##MODULE_NAME##/${MODULE_NAME}/g" \
+        -e "s,##CBIOPORTAL_CONF##,$CBIOPORTAL_CONF," \
 	-e "s/##CBIOPORTALDB_USER##/${CBIOPORTALDB_USER}/"\
         -e "s/##CBIOPORTALDB_PW##/${CBIOPORTALDB_PW}/"\
         -e "s/##CBIOPORTALDBROOT_PW##/${CBIOPORTALDBROOT_PW}/"\
@@ -94,19 +100,19 @@ case $VERB in
     sed -e "s/##PREFIX##/${PREFIX}/" \
         -e "s/##LDAPORG##/${LDAPORG}/"\
         -e "s/##LDAP_PW##/${LDAP_PW}/"\
-        -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/" \
+        -e "s/##MODULE_NAME##/${MODULE_NAME}/" \
         -e "s/##CBIOPORTALDB##/${CBIOPORTALDB}/"\
         -e "s/##CBIOPORTALDB_URL##/${CBIOPORTALDB_URL}/"\
         -e "s/##CBIOPORTALDB_USER##/${CBIOPORTALDB_USER}/"\
-        -e "s/##CBIOPORTALDB_PW##/${CBIOPORTALDB_PW}/"  templates/portal.properties_template > ${CBIOPORTAL_CONF}/portal.properties
+        -e "s/##CBIOPORTALDB_PW##/${CBIOPORTALDB_PW}/"  etc/portal.properties_template > ${CBIOPORTAL_CONF}/portal.properties
     
-#    sed -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/"  templates/app.json-template > $RF/src.cbioportal/app.json
-#    sed -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/"  templates/pom.xml-template > $RF/src.cbioportal/pom.xml
+#    sed -e "s/##MODULE_NAME##/${MODULE_NAME}/"  templates/app.json-template > $RF/src.cbioportal/app.json
+#    sed -e "s/##MODULE_NAME##/${MODULE_NAME}/"  templates/pom.xml-template > $RF/src.cbioportal/pom.xml
 #    cp templates/version.sh $RF/
-#    sed -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/"  templates/Dockerfile-template > $RF/Dockerfile
+#    sed -e "s/##MODULE_NAME##/${MODULE_NAME}/"  templates/Dockerfile-template > $RF/Dockerfile
     
 #    sed -e "s/##PREFIX##/${PREFIX}/" \
-#        -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/"\
+#        -e "s/##MODULE_NAME##/${MODULE_NAME}/"\
 #        -e "s/##CBIOPORTALDB_URL##/${CBIOPORTALDB_URL}\/$CBIOPORTALDB/"\
 #        -e "s/##CBIOPORTALDB##/${CBIOPORTALDB}/"\
 #        -e "s/##CBIOPORTALDB_USER##/${CBIOPORTALDB_USER}/"\
@@ -122,7 +128,7 @@ case $VERB in
 #        -e "s/##OUTERHOST##/${OUTERHOST}/" templates/settings.xml_template > $RF/settings.xml
 #    
 #    sed -e "s/##PREFIX##/${PREFIX}/" \
-#        -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/" \
+#        -e "s/##MODULE_NAME##/${MODULE_NAME}/" \
 #        -e "s/##CBIOPORTALDB##/${CBIOPORTALDB}/"\
 #        -e "s/##RF##/${RFS}/"\
 #        -e "s/##CBIOPORTALDB_USER##/${CBIOPORTALDB_USER}/"\
@@ -140,22 +146,22 @@ case $VERB in
 
       sed -e "s/##LDAPORG##/$LDAPORG/" \
           -e "s/##SLAPD_PASSWORD##/$LDAP_PW/" \
-          -e "s/##LDAPHOST##/${PREFIX}-${CBIOPORTAL_NAME}-ldap/" \
+          -e "s/##LDAPHOST##/${PREFIX}-${MODULE_NAME}-ldap/" \
           -e "s/##LDAPPORT##/$LDAPPORT/" scripts/addgroup.sh_template > $RF/addgroup.sh
       sed -e "s/##LDAPORG##/$LDAPORG/" \
           -e "s/##SLAPD_PASSWORD##/$LDAP_PW/" \
-          -e "s/##LDAPHOST##/${PREFIX}-${CBIOPORTAL_NAME}-ldap/" \
+          -e "s/##LDAPHOST##/${PREFIX}-${MODULE_NAME}-ldap/" \
           -e "s/##LDAPPORT##/$LDAPPORT/" scripts/adduser.sh_template > $RF/adduser.sh
           
 
       sed -e "s/##LDAPORG##/$LDAPORG/" \
           -e "s/##SLAPD_PASSWORD##/$LDAP_PW/" \
-          -e "s/##LDAPHOST##/${PREFIX}-${CBIOPORTAL_NAME}-ldap/" \
+          -e "s/##LDAPHOST##/${PREFIX}-${MODULE_NAME}-ldap/" \
           -e "s/##LDAPPORT##/$LDAPPORT/" scripts/init.sh-template > $RF/init.sh
           
       sed -e "s/##LDAPORG##/$LDAPORG/" \
           -e "s/##SLAPD_PASSWORD##/$LDAP_PW/" \
-          -e "s/##LDAPHOST##/${PREFIX}-${CBIOPORTAL_NAME}-ldap/" \
+          -e "s/##LDAPHOST##/${PREFIX}-${MODULE_NAME}-ldap/" \
           -e "s/##LDAPPORT##/$LDAPPORT/" scripts/init-core.sh-template > $RF/init-core.sh
 
       docker-compose $DOCKER_HOST -f $DOCKER_COMPOSE_FILE build
@@ -163,12 +169,13 @@ case $VERB in
 
   ;;
   "install")
-# OUTER-NGINX
-    sed -e "s/##PREFIX##/$PREFIX/"\
-        -e "s/##CBIOPORTAL_NAME##/${CBIOPORTAL_NAME}/g" \
-	    outer-nginx-${MODULE_NAME}-template > $CONF_DIR/outer_nginx/sites-enabled/${MODULE_NAME}
-#        docker $DOCKERARGS restart $PREFIX-outer-nginx
 
+  ;;
+  "install-nginx")
+    register_nginx $MODULE_NAME
+  ;;
+  "uninstall-nginx")
+    unregister_nginx $MODULE_NAME
   ;;
   "start")
     echo "Starting container ${PREFIX}-${MODULE_NAME}"
@@ -177,22 +184,32 @@ case $VERB in
   ;;
   "init")
 #    echo "Initializing slapd $PROJECT-ldap [$LDAPIP]"
-#    docker exec ${PREFIX}-${CBIOPORTAL_NAME}-ldap bash -c /init.sh
-#    docker exec ${PREFIX}-${CBIOPORTAL_NAME}-ldap bash -c /init-core.sh
-#    docker exec ${PREFIX}-${CBIOPORTAL_NAME}-ldap bash -c "/usr/local/bin/addgroup.sh users 1000"
-#    docker exec ${PREFIX}-${CBIOPORTAL_NAME}-ldap bash -c "/usr/local/bin/addgroup.sh report 9990"
+#    docker exec ${PREFIX}-${MODULE_NAME}-ldap bash -c /init.sh
+#    docker exec ${PREFIX}-${MODULE_NAME}-ldap bash -c /init-core.sh
+#    docker exec ${PREFIX}-${MODULE_NAME}-ldap bash -c "/usr/local/bin/addgroup.sh users 1000"
+#    docker exec ${PREFIX}-${MODULE_NAME}-ldap bash -c "/usr/local/bin/addgroup.sh report 9990"
   
-    #docker exec ${PREFIX}-hub-mysql bash -c "echo 'show databases' | mysql -u root --password=$HUBDBROOT_PW -h $PREFIX-hub-mysql  | grep  -q $HUBDB"
-#       docker exec ${PREFIX}-hub-mysql bash -c "echo 'use $HUBDB' | mysql -u root --password=$HUBDBROOT_PW -h $PREFIX-hub-mysql"
-#       if [ ! $? -eq 0 ];then
-    docker exec ${PREFIX}-${CBIOPORTAL_NAME}-mysql bash -c " echo \"CREATE DATABASE $DB; CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PW'; GRANT ALL ON $DB.* TO '$DB_USER'@'%';\" |  \
-             mysql -u root --password=$DBROOT_PW  -h $PREFIX-${CBIOPORTAL_NAME}-mysql"
-#       fi
 
-    docker exec ${PREFIX}-${CBIOPORTAL_NAME}-mysql bash -c "echo 'use $DB' | mysql -u root --password=$DBROOT_PW -h $PREFIX-${CBIOPORTAL_NAME}-mysql"
-    docker exec ${PREFIX}-${CBIOPORTAL_NAME}-admin python3 /cbioportal/cbioportal/manage.py makemigrations
-    docker exec ${PREFIX}-${CBIOPORTAL_NAME}-admin python3 /cbioportal/cbioportal/manage.py migrate
-    docker exec -it ${PREFIX}-${CBIOPORTAL_NAME}-admin python3 /cbioportal/cbioportal/manage.py createsuperuser
+    docker exec ${PREFIX}-${MODULE_NAME}-mysql bash -c "echo 'show databases' | mysql -u root --password=$CBIOPORTALDBROOT_PW -h $PREFIX-${MODULE_NAME}-mysql | grep  -q $CBIOPORTALDB" ||\
+       if [ ! $? -eq 0 ];then
+    docker exec ${PREFIX}-${MODULE_NAME}-mysql bash -c " echo \"CREATE DATABASE $DB; CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PW'; GRANT ALL ON $DB.* TO '$DB_USER'@'%';\" |  \
+             mysql -u root --password=$DBROOT_PW  -h $PREFIX-${MODULE_NAME}-mysql"
+       fi
+    docker cp $RF/cgds.sql ${PREFIX}-${MODULE_NAME}-mysql:/tmp/
+    docker exec ${PREFIX}-${MODULE_NAME}-mysql bash -c "mysql -u root --password=$CBIOPORTALDBROOT_PW $CBIOPORTALDB < /tmp/cgds.sql"
+    docker exec ${PREFIX}-${MODULE_NAME} bash -c "python3 /cbioportal/core/src/main/scripts/migrate_db.py -y --properties-file /cbioportal/portal.properties --sql /cbioportal/db-scripts/src/main/resources/migration.sql"
+    echo "${PREFIX}-${MODULE_NAME} Mysql (cbio) is setup."
+
+
+    docker exec ${PREFIX}-${MODULE_NAME}-mysql bash -c "echo 'show databases' | mysql -u root --password=$CBIOPORTALDBROOT_PW -h $PREFIX-${MODULE_NAME}-mysql | grep  -q $DB" ||\
+       if [ ! $? -eq 0 ];then
+    docker exec ${PREFIX}-${MODULE_NAME}-mysql bash -c " echo \"CREATE DATABASE $DB; CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PW'; GRANT ALL ON $DB.* TO '$DB_USER'@'%';\" |  \
+             mysql -u root --password=$DBROOT_PW  -h $PREFIX-${MODULE_NAME}-mysql"
+       fi
+    docker exec ${PREFIX}-${MODULE_NAME}-admin python3 /cbioportal/cbioportal/manage.py makemigrations
+    docker exec ${PREFIX}-${MODULE_NAME}-admin python3 /cbioportal/cbioportal/manage.py migrate
+    docker exec -it ${PREFIX}-${MODULE_NAME}-admin python3 /cbioportal/cbioportal/manage.py createsuperuser
+    echo "${PREFIX}-${MODULE_NAME} Mysql (admin) is setup."
 
   ;;
   "stop")
@@ -207,17 +224,16 @@ case $VERB in
 
   ;;
   "purge")
-  echo "Cleaning cbioportal folder $SRV/; Remove aquota"
-#  quotaoff -vu $SRV
-#  quotaoff -vg $SRV
-#  rm -f $SRV/aquota.*
-  rm -r $SRV/.secrets
+  echo "Cleaning cbioportal folder $SRV/${MODULE_NAME}"
+  
+    rm -r ${CBIOPORTAL_DIR}* $CBIOPORTAL_LOG $CBIOPORTAL_CONF
+    docker $DOCKERARGS volume rm  ${PREFIX}-${MODULE_NAME}-admin ${PREFIX}-${MODULE_NAME}-ldap-etc ${PREFIX}-${MODULE_NAME}-ldap-var\
+           ${PREFIX}-${MODULE_NAME}-seeddb ${PREFIX}-${MODULE_NAME}-mysqldb ${PREFIX}-${MODULE_NAME}-studies\
+	   ${PREFIX}-${MODULE_NAME}-log
+
   ;;
   "clean")
     echo "Cleaning cbioportal image ${PREFIX}-cbioportal"
-    #umount $SRV 
-#    echo "Check if $SRV is still mounted! Then run: ' rm -f "$DISKIMG/$PROJECT"fs.img '" 
-    #rm -f $DISKIMG/$PROJECT"fs.img" 
     docker $DOCKERARGS rmi ${PREFIX}-cbioportal
   ;;
 esac
